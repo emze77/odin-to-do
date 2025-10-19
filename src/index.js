45dbefbb0608f849265818247fe9f1b5547e6ec7
emze <kontakt@marcus-sperber.de>
2025-10-19 22:31:17 +0200

    numbering buttons from 0...

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import "./styles.css";
 import "./classes.js";
 
 
+const buttons = {
+    projects: document.querySelectorAll("#nav__projects"),
+    // upper Row: 1 - 4, middle row: 6 - 10, lower row: 12 - 16
+    deck: document.querySelectorAll("#deck"),
+}
+
+buttons.projects.forEach((item) => {
+    item.addEventListener('click', () => {
+        openEntryDialog(project);
+
+    })
+})j
